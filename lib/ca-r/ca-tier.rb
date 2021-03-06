6297896749f00a0b2a148f10d3b11b1298fa5c80
Daniel Kelley <dkelley@gmp.san-jose.ca.us>
2021-03-06 11:57:01 -0800

    Fix incorrect population data
    "Blueprint For A Safer Economy" CSV columns have changed incompatibly
    since initial development. CSV parsing and data extraction is made
    (hopefully) more robust against CSV format changes.
    
    lib/ca-r/app-data.rb
    
      Add get_tier_value so missing tier keys can be checked
    
    lib/ca-r/ca-tier.rb
    
      Add CSV header debugging code and reconstruct CSV header so that it
      can be parsed by the CSV library and used to facilitate column data
      extraction.
    
      Column data is now extracted by partial matching of header strings.

--- lib/ca-r/ca-tier.rb
+++ lib/ca-r/ca-tier.rb
@@ -46,59 +46,84 @@ class CA_Tier
   end
 
   #
+  # Groom the header so CSV can parse it.
+  #
+  def groom_header(hdr)
+    @hdr_str = hdr.join(' ').chomp
+    @hdr_str.gsub!("\n",'')
+    r = []
+    CSV.new(@hdr_str).each do |row|
+      r << row
+    end
+    raise 'oops' if r.length != 1
+    @hdr=r[0]
+  end
+
+  def lookup(key, row)
+    @hdr.each_with_index do |h,i|
+      if h =~ key
+        return row[i]
+      end
+    end
+    raise "Cannot find header key #{s}"
+  end
+
+  #
   # Add the CSV row to @data
+  #
+  # Only add data needed for app_data to improve robustness over header
+  # changes.
   #
   def add_row(row)
     #pp row
-    @data[row[0]] = {
-      'assessment_date' => row[1],
-      'ending_date' => row[2],
-      'final_tier' => groom_integer(row[3]),
-      'previous_tier' => groom_integer(row[4]),
-      'starting_date' => row[5],
-      'current_tier' => groom_integer(row[6]),
-      'test_positivity' => groom_float(row[7]),
-      'adjusted_case_rate' => groom_float(row[8]),
-      'unadjusted_case_rate' => groom_float(row[9]),
-      'adjustment_factor' => groom_float(row[10]),
-      'tests_per_100k' => groom_float(row[11]),
-      'population' => groom_integer(row[12]),
-      'HEQ_positivity' => groom_float(row[13]),
+    @data[lookup(/County/,row)] = {
+      # 'ROW' => row,
+      # 'assessment_date' => lookup(/Date of Tier Assessment/,row),
+      # 'ending_date' => lookup(/Ending Date/,row),
+      'final_tier' => groom_integer(lookup(/Final Tier/,row)),
+      'previous_tier' => groom_integer(lookup(/Previous Tier/,row)),
+      #'starting_date' => lookup(//,row),
+      'current_tier' => groom_integer(lookup(/Tier for Week/,row)),
+      'test_positivity' => groom_float(lookup(/Test Positivity/,row)),
+      #'adjusted_case_rate' => groom_float(lookup(//,row)),
+      #'unadjusted_case_rate' => groom_float(lookup(//,row)),
+      #'adjustment_factor' => groom_float(lookup(//,row)),
+      'tests_per_100k' => groom_float(lookup(/Tests per 100,000/,row)),
+      'population' => groom_integer(lookup(/Population/,row)),
+      #'HEQ_positivity' => groom_float(lookup(//,row)),
     }
   end
 
   # WAR for malformed CSV header: just skip header until the first county
   def groom(csv)
+    h = []
     a = []
     skip = true
     csv.each_line do |line|
       if line =~ /Alameda/
         skip = false
       end
-      a << line if !skip
+      if skip
+        h << line
+      else
+        a << line
+      end
     end
-    a.join("\r\n")
+    groom_header(h)
+    body = a.join("\r\n")
+    body
   end
 
   # Scan chart data and convert to internal format
+  #
+
+  # CSV Header is a moving target. Just like CA Covid-19 eligibility
+  # requirements. This might explain things.
   def scan_chart(csv)
-    CSV.new(groom(csv)).each do |row|
+    body = groom(csv)
+    CSV.new(body).each do |row|
       error = 0
-      county = row[0]
-      #  0 County
-      #  1 Date of Tier Assessment
-      #  2 Ending Date of Week of Data
-      #  3 Final Tier Assignment
-      #  4 Previous Tier Assignment
-      #  5 First Date in Current Tier
-      #  6 Tier for Week
-      #  7 Test Positivity
-      #  8 Case Rate Used for Tier Adjusted Using Linear Adjustment
-      #  9 Unadjusted Case Rate per 100,000
-      # 10 Linear Adjustment Factor Applied to Case Rate
-      # 11 Tests per 100,000
-      # 12 Population
-      # 13 Health Equity Quartile Test Positivity
+      county = lookup(/County/, row)
       next if row.length == 0
       next if skip_row(county)
       raise "oops #{row.inspect}" if county.nil?
@@ -141,9 +166,20 @@ class CA_Tier
     return self
   end
 
-  # Generate YAML data
+  # Save header info for debugging
+  def debug_hdr(dir)
+    d = [@hdr.length,@hdr, @hdr_str]
+    File.open("#{dir}/ca-tier-hdr.yml", "w") do |f|
+      f.puts(d.to_yaml)
+    end
+  end
+
+
+  # Generate YAML and header debug data
   def generate(out)
+    dir = File.dirname(out)
     File.open(out, "w") { |f| f.puts(@data.to_yaml) }
+    debug_hdr(dir)
   end
 
 end
